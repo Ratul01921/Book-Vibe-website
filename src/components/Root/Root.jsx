0fef87bf3b81b8c2f4e1adcb1e59c5fd25b5ecf1
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-11-03 21:02:47 +0600

    project inviornemnt setup complete

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+          <h1>welcome toh boiPoka react website created by Shahriar Ratul</h1>  
+        </div>
+    );
+};
+
+export default Root;
